ba45bf7f527d5e64bc20e5dd8c92157da6c00743
Rajendarreddy Jagapathi <rajendarreddyj@users.noreply.github.com>
2018-04-20 15:30:24 -0500

    Update smart-app-banner.js

--- docs/smart-app-banner.js
+++ docs/smart-app-banner.js
@@ -107,12 +107,12 @@
                     q.className = "smartbanner smartbanner-" + (this.options.theme || this.type);
                     q.innerHTML = '<div class="smartbanner-container"><a href="javascript:void(0);" class="smartbanner-close">&times;</a><span class="smartbanner-icon" style="background-image: url(' + m + ')"></span><div class="smartbanner-info"><div class="smartbanner-title">' +
                         this.options.title + "</div><div>" + this.options.author + "</div><span>" + e + '</span></div><a onclick=' +
-                    'gotomobileapp()'
+                    '"gotomobileapp() "'
                      +'class="smartbanner-button"><span class="smartbanner-button-text">' + this.options.button + "</span></a></div>";
                     a.body ? a.body.appendChild(q) : a && a.addEventListener("DOMContentLoaded", function() {
                         a.body.appendChild(q)
                     });
-                    //b(".smartbanner-button", q).addEventListener("click", this.install.bind(this), !1);
+                    b(".smartbanner-button", q).addEventListener("click", this.install.bind(this), !1);
                     b(".smartbanner-close", q).addEventListener("click", this.close.bind(this), !1)
                 },
                 hide: function() {
